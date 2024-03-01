ab9b7e34f80b7c041ae4cacd8093f5635491b319
Reginaldo Villela <43396103+reginaldovillela@users.noreply.github.com>
2024-02-29 23:26:48 -0300

    blog's second version

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  @Input() id: string = '0';
+  @Input() photoCover: string = '';
+  @Input() cardTitle: string = '';
+  @Input() cardDescription: string = '';
 
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input() id: string = '0';
+  @Input() photoCover: string = '';
+  @Input() cardDescription: string = '';
 
+  ngOnInit(): void {}
 }
